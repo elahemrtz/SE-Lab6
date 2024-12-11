2657a72864f605d78e60de0cbb7411dd90f26c29
dorrin-sot <dorrinsotoudeh123@gmail.com>
2024-12-11 18:20:55 +0330

    #21 2/4 get_var done

--- app.py
+++ app.py
@@ -14,5 +14,15 @@ def set_var():
 	return 'variable created', 201
 
 
+@app.route('/get_var', methods=['GET'])
+def get_var():
+	global variables
+	body = request.json
+	key = body['key']
+	if key not in variables.keys():
+		return 'variable not found', 404
+	return variables[key], 200
+
+
 if __name__ == '__main__':
 	app.run()
