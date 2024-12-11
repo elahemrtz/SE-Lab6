77c029c34dc38a86eeed747f32574e9936ded643
fatemeh-fo <63150708+fatemeh-fo@users.noreply.github.com>
2024-12-11 19:26:12 +0000

    #26 #4 integrate db into backend and added docker compose

--- backend/app.py
+++ backend/app.py
@@ -1,52 +1,65 @@
+import sys
+
 from flask import Flask, request
+
+from db import key_exists, initialize_db, exec_query
 
 app = Flask(__name__)
 
-variables = {}
+
+@app.route('/healthcheck', methods=['GET'])
+def healthcheck():
+	return 'server is healthy', 200
 
 
 @app.route('/set_var', methods=['POST'])
 def set_var():
-	global variables
 	body = request.json
 	key, value = body['key'], body['value']
-	if key in variables.keys():
+	if key_exists(key):
 		return 'variable already exists', 403
-	variables[key] = value
+
+	exec_query('INSERT INTO public.variables VALUES(%s, %s)', (key, value))
+
 	return 'variable created', 201
 
 
 @app.route('/get_var', methods=['GET'])
 def get_var():
-	global variables
 	body = request.json
 	key = body['key']
-	if key not in variables.keys():
+
+	if not key_exists(key):
 		return 'variable not found', 404
-	return variables[key], 200
+
+	return exec_query('SELECT value from public.variables WHERE key=%s', (key,), return_value=True)[0], 200
 
 
 @app.route('/edit_var', methods=['PUT'])
 def edit_var():
-	global variables
 	body = request.json
 	key, value = body['key'], body['value']
-	if key not in variables.keys():
+
+	if not key_exists(key):
 		return 'variable not found', 404
-	variables[key] = value
+
+	exec_query('UPDATE public.variables SET value=%s WHERE key=%s', (value, key))
 	return 'variable edited', 200
 
 
 @app.route('/delete_var', methods=['DELETE'])
 def delete_var():
-	global variables
 	body = request.json
 	key = body['key']
-	if key not in variables.keys():
+
+	if not key_exists(key):
 		return 'variable not found', 404
-	del variables[key]
+
+	exec_query('DELETE FROM public.variables WHERE key=%s', (key,))
+
 	return 'variable deleted', 204
 
 
+initialize_db()
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
